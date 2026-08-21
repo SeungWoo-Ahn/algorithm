e0e4dea7846cf7155b686a72c58506eac122fba7
안승우 <78468001+SeungWoo-Ahn@users.noreply.github.com>
2026-08-21 18:24:44 +0900

    [level 3] Title: 등굣길, Time: 0.40 ms, Memory: 57.5 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -6,13 +6,18 @@ class Solution {
             isPuddle[puddle[1] - 1][puddle[0] - 1] = true;
         }
         dp[0][0] = 1;
-        for (int x = 0; x < dp.length; x++) {
-            for (int y = 0; y < dp[x].length; y++) {
+        for (int y = 0; y < m; y++) {
+            if (isPuddle[0][y]) break;
+            dp[0][y] = 1;
+        }
+        for (int x = 0; x < n; x++) {
+            if (isPuddle[x][0]) break;
+            dp[x][0] = 1;
+        }
+        for (int x = 1; x < n; x++) {
+            for (int y = 1; y < m; y++) {
                 if (isPuddle[x][y]) continue;
-                if (x == 0 && y == 0) continue;
-                int top = x > 0 ? dp[x - 1][y] : 0;
-                int left = y > 0 ? dp[x][y - 1] : 0;
-                dp[x][y] = (top + left) % 1_000_000_007;
+                dp[x][y] = (dp[x - 1][y] + dp[x][y - 1]) % 1_000_000_007;
             }
         }
         return dp[n - 1][m - 1];
