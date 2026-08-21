7b3b9ba52e502cfa9cad71247747925f1302d900
안승우 <78468001+SeungWoo-Ahn@users.noreply.github.com>
2026-08-21 16:23:18 +0900

    [level 3] Title: 네트워크, Time: 1.53 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,45 +1,41 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.ArrayDeque;
+import java.util.*;
 
 class Solution {
-    private List<Integer>[] getAdj(int n, int[][] computers) {
-        List<Integer>[] adj = new List[n];
-        for (int i = 0; i < adj.length; i++) {
+    private List<Integer>[] adj;
+    private boolean[] v;
+    
+    private void bfs(int st) {
+        Deque<Integer> dq = new ArrayDeque<>();
+        dq.addLast(st);
+        v[st] = true;
+        while (!dq.isEmpty()) {
+            int cur = dq.removeFirst();
+            for (int nxt : adj[cur]) {
+                if (v[nxt]) continue;
+                dq.addLast(nxt);
+                v[nxt] = true;
+            }
+        }
+    }
+    
+    public int solution(int n, int[][] computers) {
+        adj = new List[n];
+        v = new boolean[n];
+        for (int i = 0; i < n; i++) {
             adj[i] = new ArrayList<>();
         }
-        for (int i = 0; i < computers.length - 1; i++) {
-            for (int j = i + 1; j < computers.length; j++) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
                 if (computers[i][j] == 1) {
                     adj[i].add(j);
                     adj[j].add(i);
                 }
             }
         }
-        return adj;
-    }
-    
-    private void bfs(int st, List<Integer>[] adj, boolean[] visited) {
-        ArrayDeque<Integer> q = new ArrayDeque<>();
-        q.offer(st);
-        visited[st] = true;
-        while (!q.isEmpty()) {
-            int cur = q.poll();
-            for (int nxt : adj[cur]) {
-                if (visited[nxt]) continue;
-                q.offer(nxt);
-                visited[nxt] = true;
-            }
-        }
-    }
-    
-    public int solution(int n, int[][] computers) {
-        List<Integer>[] adj = getAdj(n, computers);
-        boolean[] visited = new boolean[n];
         int result = 0;
         for (int st = 0; st < n; st++) {
-            if (visited[st]) continue;
-            bfs(st, adj, visited);
+            if (v[st]) continue;
+            bfs(st);
             result++;
         }
         return result;
