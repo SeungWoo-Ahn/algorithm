ff478364fecbbf3a660082d37342b7c13d993f8a
안승우 <78468001+SeungWoo-Ahn@users.noreply.github.com>
2026-08-24 14:24:08 +0900

    [level 3] Title: 스티커 모으기(2), Time: 4.72 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -1,34 +1,34 @@
 class Solution {
-    private int useFirst(int[] stickers) {
-        int size = stickers.length - 1;
-        int[] dp = new int[size];
+    private int selectFirst(int[] stickers) {
+        int n = stickers.length;
+        int[] dp = new int[n];
         dp[0] = stickers[0];
         dp[1] = Math.max(stickers[0], stickers[1]);
-        for (int i = 2; i < dp.length; i++) {
-            dp[i] = Math.max(dp[i - 2] + stickers[i], dp[i - 1]);
+        for (int i = 2; i < n - 1; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + stickers[i]);
         }
-        return dp[size - 1];
+        return dp[n - 2];
     }
     
-    private int useNotFirst(int[] stickers) {
-        int size = stickers.length;
-        int[] dp = new int[size];
+    private int notSelectFirst(int[] stickers) {
+        int n = stickers.length;
+        int[] dp = new int[n];
         dp[1] = stickers[1];
         dp[2] = Math.max(stickers[1], stickers[2]);
-        for (int i = 3; i < dp.length; i++) {
-            dp[i] = Math.max(dp[i - 2] + stickers[i], dp[i - 1]);
+        for (int i = 3; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + stickers[i]);
         }
-        return dp[size - 1];
+        return dp[n - 1];
     }
     
     public int solution(int[] stickers) {
         if (stickers.length < 3) {
             int max = 0;
-            for (int sticker : stickers) {
-                max = Math.max(max, sticker);
+            for (int s : stickers) {
+                max = Math.max(max, s);
             }
             return max;
         }
-        return Math.max(useFirst(stickers), useNotFirst(stickers));
+        return Math.max(selectFirst(stickers), notSelectFirst(stickers));
     }
 }
