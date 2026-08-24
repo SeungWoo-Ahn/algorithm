54f724a14b2f2cbda01e127f94e86d1ad32976b7
안승우 <78468001+SeungWoo-Ahn@users.noreply.github.com>
2026-08-24 16:11:24 +0900

    [level 3] Title: 가장 먼 노드, Time: 14.26 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,65 +1,33 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.*;
 
 class Solution {
-    private List<Integer>[] getAdj(int n, int[][] edges) {
+    public int solution(int n, int[][] edge) {
         List<Integer>[] adj = new List[n + 1];
-        for (int i = 1; i < adj.length; i++) {
+        for (int i = 1; i <= n; i++) {
             adj[i] = new ArrayList<>();
         }
-        for (int[] edge : edges) {
-            int a = edge[0];
-            int b = edge[1];
-            adj[a].add(b);
-            adj[b].add(a);
+        for (int[] e : edge) {
+            adj[e[0]].add(e[1]);
+            adj[e[1]].add(e[0]);
         }
-        return adj;
-    }
-    
-    private int[] bfs(List<Integer>[] adj) {
         Queue<Integer> q = new LinkedList<>();
-        boolean[] visited = new boolean[adj.length];
-        int[] d = new int[adj.length];
-        q.offer(1);
-        visited[1] = true;
+        boolean[] v = new boolean[n + 1];
+        q.add(1);
+        v[1] = true;
         while (!q.isEmpty()) {
-            int cur = q.poll();
-            for (int nxt : adj[cur]) {
-                if (visited[nxt]) continue;
-                q.offer(nxt);
-                visited[nxt] = true;
-                d[nxt] = d[cur] + 1;
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int cur = q.poll();
+                for (int nxt : adj[cur]) {
+                    if (v[nxt]) continue;
+                    q.add(nxt);
+                    v[nxt] = true;
+                }
+            }
+            if (q.isEmpty()) {
+                return size;
             }
         }
-        return d;
-    }
-    
-    private int getMax(int[] arr) {
-        int max = 0;
-        for (int num : arr) {
-            if (num > max) {
-                max = num;
-            }
-        }
-        return max;
-    }
-    
-    private int getCnt(int target, int[] arr) {
-        int cnt = 0;
-        for (int num : arr) {
-            if (num == target) {
-                cnt++;
-            }
-        }
-        return cnt;
-    }
-    
-    public int solution(int n, int[][] edges) {
-        List<Integer>[] adj = getAdj(n, edges);
-        int[] d = bfs(adj);
-        int max = getMax(d);
-        return getCnt(max, d);
+        return 0;
     }
 }
