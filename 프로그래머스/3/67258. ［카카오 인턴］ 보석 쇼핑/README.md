c64310fc3e200593d6f71e9f5547eef7f99b634c
안승우 <78468001+SeungWoo-Ahn@users.noreply.github.com>
2026-08-24 15:07:01 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 26.30 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/README.md
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/README.md
@@ -1,10 +1,10 @@
 # [level 3] [카카오 인턴] 보석 쇼핑 - 67258 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67258?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67258) 
 
 ### 성능 요약
 
-메모리: 81.2 MB, 시간: 69.15 ms
+메모리: 82.9 MB, 시간: 26.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 09일 17:39:16
+2026년 08월 24일 15:06:57
 
 ### 문제 설명
 
--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -1,51 +1,32 @@
-import java.util.Set;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
-    private int getGemsCount(String[] gems) {
+    public int[] solution(String[] gems) {
         Set<String> set = new HashSet<>();
         for (String gem : gems) {
             set.add(gem);
         }
-        return set.size();
-    }
-    
-    public int[] solution(String[] gems) {
         Map<String, Integer> map = new HashMap<>();
-        int[] result = new int[2];
-        int gemsCount = getGemsCount(gems);
-        int st = 0;
-        int en = 0;
-        int size = gems.length;
-        while (st < gems.length) {
-            if (map.size() == gemsCount) {
-                if (en - st < size) {
-                    result[0] = st + 1;
-                    result[1] = en;
-                    size = en - st;
-                } else {
-                    int count = map.getOrDefault(gems[st], 0);
-                    if (count <= 1) {
-                        map.remove(gems[st]);
-                    } else {
-                        map.put(gems[st], count - 1);
-                    }
-                    st++;
-                }
-            } else {
-                if (en < gems.length) {
-                    map.put(gems[en], map.getOrDefault(gems[en], 0) + 1);
-                    en++;
-                } else {
-                    break;
-                }
+        int kind = set.size();
+        int left = 0;
+        int right = 0;
+        int[] result = new int[]{1, gems.length};
+        map.put(gems[0], 1);
+        while (left <= right) {
+            if (map.size() == kind && right - left < result[1] - result[0]) {
+                result[0] = left + 1;
+                result[1] = right + 1;
             }
-        }
-        if (result[0] == 0 && result[1] == 0) {
-            result[0] = 1;
-            result[1] = gems.length;
+            if (map.size() < kind) {
+                if (++right == gems.length) break;
+                map.put(gems[right], map.getOrDefault(gems[right], 0) + 1);
+            } else {
+                map.put(gems[left], map.get(gems[left]) - 1);
+                if (map.get(gems[left]) == 0) {
+                    map.remove(gems[left]);
+                }
+                left++;
+            }
         }
         return result;
     }
